40a60e739470fc29dd8d89d91dca23a25fc55420
JosephWang <joseph2903106@gmail.com>
2022-05-17 14:53:21 +0800

    feature: init react

---
+++ out/demo1.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var x = 5;
+x += 1;
+var y = 'Hello world';
+console.log(x);
+console.log(y);
---
+++ src/demo1.js
@@ -0,0 +1,5 @@
+let x = 5 
+x+=1
+const y ='Hello world'
+console.log(x)
+console.log(y)
